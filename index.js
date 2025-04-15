8b34f8db13ac8c535b6154393ad0eb5ec5be87e8
Yashodha8890 <yashodha23000@student.hamk.fi>
2025-04-15 23:20:39 +0300

    added handlebars files and static pages.

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 
 const app = express();
+
+//this code says that defaults settings we are using the main.handlebars
+app.engine('handlebars', exphbs.engine({
+    defaultLayout: 'main'
+}));
+
+//This is to indicate we use handlebars further
+app.set('view engine', 'handlebars');
+
 app.get('/',(req,res) => {
-    res.send("Testing");
+    //res.send("Testing");
+    res.render('index');
 });
+
+app.get('/contact',(req,res) => {
+    res.render('contact');
+});
+
+//This is the folder which contains files like css, imgs
+app.use(express.static('public'));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
